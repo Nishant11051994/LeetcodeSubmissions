95b840cbd08820cc15df2519338a00a10605b714
Nishant <54793747+0Indian@users.noreply.github.com>
2020-12-07 20:06:55 +0530

    working commit - Created using LeetHub

---
+++ longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int LengthOfLongestSubstringTwoDistinct(string s) 
+    {
+            if(string.IsNullOrEmpty(s)) return 0;
+            Dictionary<char, int> freqMap = new Dictionary<char, int>();
+            int start = 0;
+            int end = 0;
+            int maxLength = 0;
+            int k = 2;
+​
+            while(end < s.Length)
+            {
+                if(!freqMap.ContainsKey(s[end]))
+                {
+                    freqMap.Add(s[end], 0);
+                }
+                freqMap[s[end]]++;                            
+                while(freqMap.Count > k)
+                {
+                    freqMap[s[start]]--;
+                    if(freqMap[s[start]] == 0)
+                    {
+                        freqMap.Remove(s[start]);
+                    }
+                    start++;
+                }
+                maxLength = Math.Max(end - start + 1, maxLength);
+                end++;
+            }
+            return maxLength;       
+    }
+}
