9d6073f36451ec4b9350f7b2b60ac95b4f5230e8
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-02-21 11:15:30 +0530

    Time: 84 ms (96.30%), Space: 25.3 MB (19.65%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
@@ -1,40 +1,34 @@
 public class Solution {
-    public int FindMin(int[] nums) 
-    {
-        if(nums == null || nums.Length == 0)
-        {
-            return -1;
-        }
-        
-        if(nums.Length == 1) return nums[0];
-        
-        if(nums[0] < nums[nums.Length-1]) return nums[0];
-        
-        int index = BinarySearch(nums,0,nums.Length-1);
-        
-        return index != -1 ? nums[index] : -1; 
-    }
-    private int BinarySearch(int[] nums,int start,int end)
-    {
-        int n = nums.Length;        
-        while(start <= end)
-        {
-            if(nums[start] <= nums[end]) return start;
-            int mid = start + (end-start)/2;
-            if(nums[mid] <= nums[(mid-1+n)%n] && nums[mid] <= nums[(mid+1)%n])
-            {
-                return mid;
-            }
-            else if(nums[mid] >= nums[start])
-            {
-                start = mid + 1;
-            }
-            else if(nums[mid] <= nums[end])
-            {
-                end = mid - 1;
-            }
-        }
-        return -1;
-    }
-    
+    public int FindMin(int[] nums) 
+    {
+        if(nums == null || nums.Length == 0) return 0;
+        
+        int pivot = FindPivot(nums);
+        
+        return nums[pivot];
+    }
+    private int FindPivot(int[] nums)
+    {
+        int start = 0;
+        int end = nums.Length-1;
+        if(nums[start] < nums[end]) return start;
+        while(start <= end)
+        {
+            int mid = start + (end - start)/2;
+            Console.WriteLine($"mid is {mid} and nums.Length-1 is {nums.Length-1}");
+            if(mid < nums.Length-1 && nums[mid] > nums[mid+1])
+            {
+                return mid + 1;
+            }
+            else if(nums[0] > nums[mid])
+            {
+               end = mid - 1;
+            }
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return 0;
+    }
 }
