cd91f2541bce8f3399a3fd0e9ac9d74815bcc71b
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-02-21 13:49:20 +0530

    Time: 244 ms (73.46%), Space: 32.5 MB (74.95%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cs
@@ -2,37 +2,43 @@ public class Solution {
     public int[] SearchRange(int[] nums, int target) 
     {
         int[] result = new int[2]{-1,-1};
-        
-        int leftMostIndex = BinarySearch(nums,target,true);
-        
-        if(leftMostIndex == nums.Length || nums[leftMostIndex] != target)
-        {
-            return result;
-        }
        
-        result[0] = leftMostIndex;
-        result[1] = BinarySearch(nums,target,false) - 1;
+        result[0] = BinarySearch(nums,target,true);
+        result[1] = BinarySearch(nums,target,false);
              
         return result;
     }
-    private int BinarySearch(int[] nums, int target, bool left)
+    private int BinarySearch(int[] nums, int target, bool leftBias)
     {
         int start = 0;
-        int end = nums.Length;
+        int end = nums.Length-1;
+        int i = -1;
         
-        while(start < end)
+        while(start <= end)
         {
             int mid = start + (end - start)/2;
-            if(nums[mid] > target || (left && target == nums[mid]))
-            {
-                end = mid;
-            }
-            else
+            if(target > nums[mid])
             {
                 start = mid + 1;
             }
+            else if(target < nums[mid])
+            {
+                end = mid - 1;
+            }
+            else
+            {
+                i = mid;
+                if(leftBias)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
         }        
-        return start;
+        return i;
         
     }
 }
