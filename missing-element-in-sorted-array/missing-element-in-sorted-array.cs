9b5afd8455ebe80e7b15d74e7b7f85a3ccaac4d2
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-02-25 08:32:56 +0530

    Time: 144 ms (85.27%), Space: 39.6 MB (72.87%) - LeetHub

--- missing-element-in-sorted-array/missing-element-in-sorted-array.cs
+++ missing-element-in-sorted-array/missing-element-in-sorted-array.cs
@@ -1,14 +1,21 @@
 public class Solution {
     public int MissingElement(int[] nums, int k) 
     {
-        int left = 0, right = nums.Length;
-        while (left < right) {
-        int mid = left + (right - left) / 2;
-        if (nums[mid] - nums[0] < mid + k)
-            left = mid + 1;
-        else
-            right = mid;
-         }
-        return nums[0] + left + k - 1;
+        int left = 0;
+        int right = nums.Length - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            int missing = nums[mid] - nums[0] - mid;
+            if(missing >= k)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return k + nums[0] + right;
     }
 }
