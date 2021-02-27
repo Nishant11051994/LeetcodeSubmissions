cda54bca1f23bb274e0529f0e1ce472435555c50
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-02-27 22:19:14 +0530

    Time: 88 ms (65.19%), Space: 23.5 MB (78.77%) - LeetHub

--- longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.cs
+++ longest-substring-with-at-most-k-distinct-characters/longest-substring-with-at-most-k-distinct-characters.cs
@@ -1,31 +1,34 @@
-//same as https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/
 public class Solution {
-    public int LengthOfLongestSubstringKDistinct(string s, int k) 
-    {
-            Dictionary<char, int> freqMap = new Dictionary<char, int>();
-            int start = 0;
-            int end = 0;
-            int maxLength = 0;
-​
-            while(end < s.Length)
-            {
-                if(!freqMap.ContainsKey(s[end]))
-                {
-                    freqMap.Add(s[end], 0);
-                }
-                freqMap[s[end]]++;            
-                while(freqMap.Count > k)
-                {
-                    freqMap[s[start]]--;
-                    if(freqMap[s[start]] == 0)
-                    {
-                        freqMap.Remove(s[start]);
-                    }
-                    start++;
-                }
-                maxLength = Math.Max(end - start + 1,maxLength);
-                end++;
-            }
-            return maxLength; 
-    }
+    public int LengthOfLongestSubstringKDistinct(string s, int k) 
+    {
+        if(string.IsNullOrEmpty(s)) return 0;
+        
+        Dictionary<char,int> freqMap = new Dictionary<char,int>();
+        
+        int start = 0;
+        int end = 0;
+        int maxLength = 0;
+        
+        while(end < s.Length)
+        {
+            if(!freqMap.ContainsKey(s[end]))
+            {
+                freqMap.Add(s[end],0);
+            }
+            freqMap[s[end]]++;           
+            
+            if(freqMap.Count > k)
+            {               
+               freqMap[s[start]]--;
+               if(freqMap[s[start]] == 0)
+               {
+                   freqMap.Remove(s[start]);
+               }
+               start++;
+            }
+            maxLength = Math.Max(maxLength,end-start+1);
+            end++;
+        }
+        return maxLength;
+    }
 }
