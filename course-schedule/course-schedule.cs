244f6d53daa0abcfed657b0623642138317d64bf
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-04-02 11:33:15 +0530

    Time: 120 ms (46.93%), Space: 30.1 MB (86.30%) - LeetHub

--- course-schedule/course-schedule.cs
+++ course-schedule/course-schedule.cs
@@ -1,27 +1,54 @@
 public class Solution {
-    List<int>[] adjencyList;
-    int n;
-    private void AddEdge(int src,int dest)
-    {
-        adjencyList[src].Add(dest);
-    }
-    public bool CanFinish(int numCourses, int[][] prerequisites) 
-    {
-        if(numCourses == null || prerequisites == null || prerequisites.Length == 0)
-        {
-            return true;
-        }
-        
-        n = numCourses;
-        adjencyList = new List<int>[n];
-        for(int i = 0 ; i < n ; i++)
-        {
-            adjencyList[i] = new List<int>();
-        }
-        
-        for(int i = 0 ; i < prerequisites.Length ; i++)
-        {
-            AddEdge(prerequisites[i][1],prerequisites[i][0]);
-        }
-        
-        int[] indegree = new int[n];
+    List<int>[] adjencyList;
+    private void AddEdge(int src,int dest)
+    {
+        adjencyList[src].Add(dest);
+    }
+    public bool CanFinish(int numCourses, int[][] prerequisites) 
+    {
+        adjencyList = new List<int>[numCourses];
+        for(int i = 0 ; i < numCourses ; i++)
+        {
+            adjencyList[i] = new List<int>();
+        }
+        for(int i = 0 ; i < prerequisites.Length ; i++)
+        {
+            AddEdge(prerequisites[i][1],prerequisites[i][0]);
+        }
+        
+        int[] indegree = new int[numCourses];
+        foreach(var item in adjencyList)
+        {
+            foreach(int course in item)
+            {
+                indegree[course]++;
+            }
+        }
+        Queue<int> queue = new Queue<int>();
+        for(int i = 0 ; i < numCourses ; i++)
+        {
+            if(indegree[i] == 0)
+            {
+                queue.Enqueue(i);
+            }
+        }
+        int count = 0;
+        while(queue.Count != 0)
+        {
+            int curr = queue.Dequeue();
+            count++;
+            foreach(var item in adjencyList[curr])
+            {
+                if(--indegree[item] == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+        }
+        if(count == numCourses)
+        {
+            return true;
+        }
+        return false;
+    }
+}
