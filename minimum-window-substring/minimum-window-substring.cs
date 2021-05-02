0b8fedb5ec899a846ad4d6a83803beddeb0b7eb8
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-05-02 17:27:50 +0530

    Time: 100 ms (76.42%), Space: 26.7 MB (41.47%) - LeetHub

--- minimum-window-substring/minimum-window-substring.cs
+++ minimum-window-substring/minimum-window-substring.cs
@@ -1,58 +1,56 @@
 public class Solution {
     public string MinWindow(string s, string t) 
     {
-       if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t)) return string.Empty;
+        if(String.IsNullOrEmpty(s) && String.IsNullOrEmpty(t)) return string.Empty;
         
-       Dictionary<char,int> freqMap = new Dictionary<char,int>();
-    
-       for(int i = 0 ; i < t.Length ; i++)
-       {
-           if(!freqMap.ContainsKey(t[i]))
-           {
-               freqMap.Add(t[i],0);
-           }
-           freqMap[t[i]]++;
-       }
+        if(t.Length > s.Length) return string.Empty;
         
+        Dictionary<char,int> tMap = new Dictionary<char,int>();
+                
+        for(int i = 0 ; i < t.Length ; i++)
+        {
+            if(!tMap.ContainsKey(t[i]))
+            {
+                tMap.Add(t[i],0);
+            }
+            tMap[t[i]]++;
+        }
         
-       int counter = freqMap.Count;
+        int size = tMap.Count;
+        int start = 0;
+        int startIndex = 0;
         
-       int start = 0;
-       int end = 0;        
-       int minLength = Int32.MaxValue;
-       int finalStart = 0;
+        int minLength = Int32.MaxValue;
         
-       while(end < s.Length)
-       {
-           if(freqMap.ContainsKey(s[end]))
-           {
-               freqMap[s[end]]--;
-               if(freqMap[s[end]] == 0)
-               {
-                   counter--;
-               }
-           }
-           end++;
-           while(counter == 0)
-           {
-               if(end - start < minLength)
-               {
-                   minLength = end - start;
-                   finalStart = start;                   
-               }               
-               if(freqMap.ContainsKey(s[start]))
-               {
-                   freqMap[s[start]]++;
-                   if(freqMap[s[start]] > 0)
-                   {
-                       counter++;
-                   }
-               }
-               start++;
-           }           
-       }        
-       return minLength == Int32.MaxValue ? "" : s.Substring(finalStart,minLength);
-       
-       
+        for(int end = 0 ; end < s.Length ; end++)
+        {
+            if(tMap.ContainsKey(s[end]))
+            {
+                tMap[s[end]]--;
+                if(tMap[s[end]] == 0)
+                {
+                    size--;
+                }
+            }
+            while(size == 0)
+            {
+                if(end-start+1 < minLength)
+                {
+                    minLength = end-start+1;
+                    Console.WriteLine($"start is {start}, end is {end} and minLength is {minLength}");
+                    startIndex = start;
+                }                           
+                if(tMap.ContainsKey(s[start]))
+                {
+                     tMap[s[start]]++;
+                     if(tMap[s[start]] > 0)
+                     {
+                         size++;
+                     }
+                }            
+                start++;
+            }
+        }   
+        return minLength == Int32.MaxValue ? "" : s.Substring(startIndex,minLength);
     }
 }
