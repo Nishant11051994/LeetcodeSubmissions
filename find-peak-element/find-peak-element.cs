dc9abbab294f7a75ad40d4642e689279cdbdd2ec
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-06-05 13:53:26 +0530

    Time: 184 ms (5.07%), Space: 25.3 MB (20.98%) - LeetHub

--- find-peak-element/find-peak-element.cs
+++ find-peak-element/find-peak-element.cs
@@ -1,55 +1,24 @@
 public class Solution {
-    public int FindPeakElement(int[] nums) 
+    public int FindPeakElement(int[] arr) 
     {
-         if(nums == null || nums.Length == 0)
-          {
-            return 0;
-          }  
-          return BinarySearch(nums);
-    }
-    private int BinarySearch(int[] nums)
-    {
-        int start = 0;
-        int end = nums.Length - 1;
-        while(start <= end)
+        if(arr == null || arr.Length == 0) return 0;
+        
+        int left = 0,right = arr.Length-1,boundaryIndex = -1;
+        
+        while(left <= right)
         {
-            int mid = start + (end - start)/2;
-            if(GreaterThanNeighbours(mid,nums))
+            int mid = left + (right - left)/2;
+            if(mid == arr.Length-1 || arr[mid] >= arr[mid+1])
             {
-                return mid;
-            }
-            if(mid >0 && nums[mid] < nums[mid-1])
-            {
-                end = mid - 1;
+                boundaryIndex = mid;
+                right = mid - 1;
             }
             else
             {
-                start = mid + 1;
+                left = mid + 1;
             }
         }
-        return 0;
-    }
-    private bool GreaterThanNeighbours(int index,int[] nums)
-    {
-        int n = nums.Length - 1;
-        if(index > 0 && index < n)
-        {
-            if(nums[index] > nums[index-1] && nums[index] > nums[index+1])                       return true;
-            
-            return false;
-        }
-        if(index > 0)
-        {
-            if(nums[index] > nums[index-1]) return true;
-            
-            return false;            
-        }
-        if(index < n)
-        {
-            if(nums[index] > nums[index+1]) return true;
-            
-            return false;
-        }
-        return false;
+        
+        return boundaryIndex;
     }
 }
