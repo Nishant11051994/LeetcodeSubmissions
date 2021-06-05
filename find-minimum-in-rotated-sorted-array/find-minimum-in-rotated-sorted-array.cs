4fb42134be3c05eca8a1cd81ec21fe1c440367cf
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-06-05 13:34:34 +0530

    Time: 140 ms (6.81%), Space: 25.3 MB (21.66%) - LeetHub

--- find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
+++ find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cs
@@ -1,32 +1,28 @@
 public class Solution {
     public int FindMin(int[] nums) 
     {
-        if(nums == null || nums.Length == 0) return 0;
-        
-        int pivot = FindPivot(nums);
-        
-        return nums[pivot];
+        return Math.Min(nums[0],nums[pivot(nums)]);
     }
-    private int FindPivot(int[] nums)
+    private int pivot(int[] nums)
     {
-        int start = 0;
-        int end = nums.Length-1;
-        if(nums[start] < nums[end]) return start;
-        while(start <= end)
+        if(nums[0] < nums[nums.Length-1]) return 0;
+        
+        int left = 0,right = nums.Length-1;
+        
+        while(left <= right)
         {
-            int mid = start + (end - start)/2;
-            Console.WriteLine($"mid is {mid} and nums.Length-1 is {nums.Length-1}");
+            int mid = left + (right - left)/2;
             if(mid < nums.Length-1 && nums[mid] > nums[mid+1])
             {
-                return mid + 1;
+                return mid+1;
             }
-            else if(nums[0] > nums[mid])
+            else if(nums[mid] < nums[0])
             {
-               end = mid - 1;
+                right = mid - 1;
             }
             else
             {
-                start = mid + 1;
+                left = mid + 1;
             }
         }
         return 0;
