67de81654a332533ec3d692aa42fb00020623cc4
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-06-05 13:54:17 +0530

    Time: 92 ms (92.89%), Space: 27.1 MB (49.41%) - LeetHub

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int PeakIndexInMountainArray(int[] arr) 
+    {
+        if(arr == null || arr.Length == 0) return 0;
+        
+        int left = 0,right = arr.Length-1,boundaryIndex = -1;
+        
+        while(left <= right)
+        {
+            int mid = left + (right - left)/2;
+            if(mid == arr.Length-1 || arr[mid] >= arr[mid+1])
+            {
+                boundaryIndex = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        
+        return boundaryIndex;
+    }
+}
