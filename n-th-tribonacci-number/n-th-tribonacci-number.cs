b79f0ba818d61c1be909578a0b053f78cef257e6
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-09-26 11:46:19 +0530

    Time: 40 ms (81.02%), Space: 15.2 MB (60.65%) - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.cs
+++ n-th-tribonacci-number/n-th-tribonacci-number.cs
@@ -1,26 +1,19 @@
 public class Solution {
-    public int Tribonacci(int n) 
-    {
-        if(n == 0) return 0;
-        
-        if(n == 1) return 1;
-        
-        if(n == 2) return 1;
-        
-        int a = 0;
-        int b = 1;
-        int c = 1;
-        
-        for(int i = 3 ;i <= n ; i++)
-        {
-            int temp = a + b + c;
-            a = b;
-            b = c;
-            c = temp;
-        }
-        
-        return c;
-        
-        
-    }
+    Dictionary<int,int> dp;
+    public int Tribonacci(int n) 
+    {
+        dp = new Dictionary<int,int>();        
+        dp.Add(0,0);
+        dp.Add(1,1);
+        dp.Add(2,1);       
+        return Recurse(n);
+    }
+    private int Recurse(int n)
+    {
+        if(!dp.ContainsKey(n))
+        {
+           dp.Add(n,Recurse(n-1) + Recurse(n-2) + Recurse(n-3));
+        }
+        return dp[n];      
+    }
 }
