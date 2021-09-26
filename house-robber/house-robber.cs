0671e485cff7ab37348b171a6eb1fac3c1aba485
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-09-26 13:00:52 +0530

    Time: 96 ms (41.99%), Space: 24.5 MB (48.54%) - LeetHub

--- house-robber/house-robber.cs
+++ house-robber/house-robber.cs
@@ -1,27 +1,22 @@
 public class Solution {
+    Dictionary<int,int> dp;
     public int Rob(int[] nums) 
     {
-        int[] dp = new int[nums.Length];
-        for(int i = 0 ; i < nums.Length ; i++)
-        {
-            dp[i] = -1;
-        }
-        return Recurse(nums,0,dp);
+        dp = new Dictionary<int,int>();
+        return Recurse(nums,0);
     }
-    private int Recurse(int[] nums, int index,int[] dp)
+    private int Recurse(int[] nums, int index)
     {
         if(index >= nums.Length) return 0;
         
-        if(dp[index] != -1) return dp[index];
-        
-        int choice1 = 0;
-        int choice2 = 0;
-        choice1 = nums[index] + Recurse(nums,index+2,dp);
-        if(index < nums.Length)
+        if(!dp.ContainsKey(index))
         {
-            choice2 = Recurse(nums,index+1,dp);
-        }
-        dp[index] = Math.Max(choice1,choice2);
-        return dp[index];
+            int choice1 = nums[index] + Recurse(nums,index + 2);
+        
+            int choice2 = Recurse(nums,index + 1);
+            
+            dp.Add(index,Math.Max(choice1,choice2));
+        }       
+        return dp[index];         
     }
 }
