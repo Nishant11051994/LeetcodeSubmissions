8e696e34d5ce2e586c353d4776964547065edfd8
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-10-15 12:18:28 +0530

    Time: 146 ms (33.26%), Space: 36.9 MB (30.00%) - LeetHub

--- minimum-window-substring/minimum-window-substring.cs
+++ minimum-window-substring/minimum-window-substring.cs
@@ -1,29 +1,30 @@
 public class Solution {
     public string MinWindow(string s, string t) 
     {
-        if(String.IsNullOrEmpty(s) && String.IsNullOrEmpty(t)) return string.Empty;
+        if(string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t)) return string.Empty;
         
         if(t.Length > s.Length) return string.Empty;
         
         Dictionary<char,int> tMap = new Dictionary<char,int>();
-                
+        
         for(int i = 0 ; i < t.Length ; i++)
         {
-            if(!tMap.ContainsKey(t[i]))
-            {
-                tMap.Add(t[i],0);
-            }
-            tMap[t[i]]++;
+               if(!tMap.ContainsKey(t[i]))
+               {
+                  tMap.Add(t[i],0); 
+               }
+               tMap[t[i]]++;
         }
         
         int size = tMap.Count;
+        
         int start = 0;
         int startIndex = 0;
-        
         int minLength = Int32.MaxValue;
-        
-        for(int end = 0 ; end < s.Length ; end++)
+                  
+        for(int end= 0 ; end < s.Length ; end++)
         {
+            // Reduce the size to check the existenc of substring with all characters
             if(tMap.ContainsKey(s[end]))
             {
                 tMap[s[end]]--;
@@ -32,25 +33,25 @@ public class Solution {
                     size--;
                 }
             }
+            // Contracting the window to minimize till allowable limit
             while(size == 0)
             {
                 if(end-start+1 < minLength)
                 {
                     minLength = end-start+1;
-                    Console.WriteLine($"start is {start}, end is {end} and minLength is {minLength}");
                     startIndex = start;
-                }                           
+                }
                 if(tMap.ContainsKey(s[start]))
                 {
-                     tMap[s[start]]++;
-                     if(tMap[s[start]] > 0)
-                     {
-                         size++;
-                     }
-                }            
+                    tMap[s[start]]++;
+                    if(tMap[s[start]] > 0)
+                    {
+                        size++;
+                    }                   
+                }
                 start++;
             }
-        }   
+        }
         return minLength == Int32.MaxValue ? "" : s.Substring(startIndex,minLength);
     }
 }
