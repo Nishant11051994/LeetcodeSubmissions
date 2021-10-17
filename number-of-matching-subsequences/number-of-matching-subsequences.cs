0a26b7b091abb38f236862f050708ab4a4328309
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-10-17 16:44:44 +0530

    Time: 1512 ms (10.43%), Space: 44.7 MB (84.35%) - LeetHub

--- number-of-matching-subsequences/number-of-matching-subsequences.cs
+++ number-of-matching-subsequences/number-of-matching-subsequences.cs
@@ -1,39 +1,36 @@
 public class Solution {
-    public int NumMatchingSubseq(string S, string[] words) 
-    {
-        if(string.IsNullOrEmpty(S)) return 0;
-        
-        int count = 0;
-        Dictionary<string,bool> map = new Dictionary<string,bool>();
-        
-        for(int i = 0 ; i < words.Length ; i++)
-        {
-            bool result = map.ContainsKey(words[i]) ? map[words[i]] : IsSubsequence(words[i],S);
-            if(result)
-               {
-                   count++;
-               }
-            map[words[i]] = result;
-        }        
-        return count;
-    }
-    private  bool IsSubsequence(string s, string t) 
-    {
-        if(string.IsNullOrEmpty(s)) return true;
-        
-        int sIndex = 0;
-        int tIndex = 0;
-        
-        int count = s.Length;
-        while((tIndex < t.Length && sIndex < s.Length) && count!=0)
-        {
-            if(s[sIndex] == t[tIndex])
-            {
-                sIndex++;
-                count--;
-            }
-            tIndex++;
-        }
-        return count == 0 ? true : false;
-    }
+    public int NumMatchingSubseq(string s, string[] words) 
+    {
+        int result = 0;
+        for(int i = 0 ; i < words.Length ; i++)
+        {
+            if(IsSubsequence(words[i],s))
+            {
+               result++; 
+            }
+        }
+        return result;
+    }
+    private bool IsSubsequence(string s, string t) 
+    {                
+        if(string.IsNullOrEmpty(s)) return true;
+        
+        if(string.IsNullOrEmpty(t)) return false;
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < s.Length && j < t.Length)
+        {
+            if(s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        
+        return i == s.Length;
+        
+        
+    }
 }
