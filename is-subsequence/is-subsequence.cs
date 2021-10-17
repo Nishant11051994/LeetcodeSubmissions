95bd1abc0474fddc7db6f176668b01870b606ee8
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-10-17 16:39:29 +0530

    Time: 88 ms (33.89%), Space: 37.2 MB (10.08%) - LeetHub

--- is-subsequence/is-subsequence.cs
+++ is-subsequence/is-subsequence.cs
@@ -1,22 +1,24 @@
 public class Solution {
-    public bool IsSubsequence(string s, string t) 
-    {
-        if(string.IsNullOrEmpty(s)) return true;
-        
-        return Recurse(s,t,0,0);
-    }
-    private bool Recurse(string s,string t,int indexS,int indexT)
-    {        
-        if(s.Length == indexS)
-        {
-           return true;
-        }
-        if(indexT > t.Length-1) return false;
-               
-        if(s[indexS] == t[indexT])
-        {
-            indexS++;
-        }        
-        return Recurse(s,t,indexS,indexT+1);  
-    }
+    public bool IsSubsequence(string s, string t) 
+    {                
+        if(string.IsNullOrEmpty(s)) return true;
+        
+        if(string.IsNullOrEmpty(t)) return false;
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < s.Length && j < t.Length)
+        {
+            if(s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        
+        return i == s.Length;
+        
+        
+    }
 }
