91dcf6841070f32fd5fd55b37959cc07421a8575
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-10-23 19:40:22 +0530

    Time: 2307 ms (5.05%), Space: 44.5 MB (7.97%) - LeetHub

--- jump-game/jump-game.cs
+++ jump-game/jump-game.cs
@@ -1,39 +1,33 @@
-public enum State
-{
-   GOOD,
-   BAD,
-   UNKNOWN
-}
 public class Solution {
-    public bool CanJump(int[] nums) 
-    {
-        if(nums == null || nums.Length == 0) return true;
-        
-        State[] dp = new State[nums.Length];
-        for(int i=0;i<nums.Length;i++)
-        {
-          dp[i] = State.UNKNOWN;
-        }
-        dp[nums.Length-1] = State.GOOD;        
-        return Jump(nums,0,dp);        
-    }
-    private bool Jump(int[] nums,int currIndex,State[] dp)
-    {
-        if(dp[currIndex] != State.UNKNOWN)
-        {
-           return dp[currIndex] == State.GOOD ? true : false;
-        }
-        int currentMaxJump = currIndex + nums[currIndex];
-        int maxJump = Math.Min(currentMaxJump,nums.Length-1);
-        for(int i = currIndex+1 ; i <= maxJump ; i++)
-        {
-            if(Jump(nums,i,dp))
-            {
-                dp[currIndex] = State.GOOD;
-                return true;                
-            }
-        }
-        dp[currIndex] = State.BAD;
-        return false;
-    }
+    Dictionary<int,bool> dp;
+    public bool CanJump(int[] nums) 
+    {
+        dp = new Dictionary<int,bool>();
+        return BackTrack(nums,0);
+    }
+    private bool BackTrack(int[] nums,int index)
+    {
+       if(index == nums.Length-1) return true;
+        
+       if(index >= nums.Length) return false;
+        
+       if(dp.ContainsKey(index))
+       {
+           return dp[index];
+       }
+        
+       int i = index+1;
+      
+       while(i <= Math.Min(index+nums[index],nums.Length-1))
+       {
+           if(BackTrack(nums,i))
+           {
+               dp.Add(index,true);
+               return dp[index];
+           }
+           i++;
+       }   
+       dp.Add(index,false); 
+       return dp[index];         
+    }
 }
