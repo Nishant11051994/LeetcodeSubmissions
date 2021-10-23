0a6580ed6544efeded05517e1f6c17ebd1432195
Nishant <54793747+Nishant11051994@users.noreply.github.com>
2021-10-23 23:09:52 +0530

    Time: 612 ms (11.50%), Space: 43.6 MB (5.06%) - LeetHub

--- jump-game-ii/jump-game-ii.cs
+++ jump-game-ii/jump-game-ii.cs
@@ -1,27 +1,23 @@
 public class Solution {
-    public int Jump(int[] nums) 
-    {
-        if(nums == null || nums.Length == 0) return 0;        
-           
-        int[] maxJumps = new int[nums.Length];
-        
-        Array.Fill(maxJumps,Int32.MaxValue-1);
-        
-        maxJumps[nums.Length-1] = 0;
-        
-        for(int i = nums.Length-2 ; i>=0 ; i--)
-        {
-            int farthestJump = Math.Min(i + nums[i],nums.Length-1);
-            if(farthestJump == nums.Length-1)
-            {
-              maxJumps[i] = 1;
-              continue;
-            }
-            for(int j = i+1 ; j <= farthestJump ; j++)
-            {
-              maxJumps[i] = Math.Min(maxJumps[j] + 1,maxJumps[i]);
-            }   
-        }
-        return maxJumps[0];
-    }
+    Dictionary<int,int> dp;
+    public int Jump(int[] nums) 
+    {
+        dp = new Dictionary<int,int>();
+        return BackTrack(nums,0);
+    }
+    private int BackTrack(int[] nums,int index)
+    {
+        if(index >= nums.Length-1) return 0;
+        
+        if(dp.ContainsKey(index)) return dp[index];
+        
+        int minJumps = Int32.MaxValue;
+        
+        for(int i = index+1 ; i <= Math.Min(index+nums[index],nums.Length-1) ; i++)
+        {
+            minJumps = Math.Min(minJumps,BackTrack(nums,i));
+        }      
+        dp[index] = minJumps == Int32.MaxValue ? Int32.MaxValue : minJumps+1;
+        return dp[index];
+    }
 }
